a0201f381f5f6ff23a27e5c71f4172dc5f46ae46
jDan735 <daniel734@bk.ru>
2021-02-23 19:39:12 +0300

    Add locales

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+    "gopkg.in/yaml.v2"
+
 	"math/rand"
+    "io/ioutil"
+    "strconv"
+	"strings"
     "flag"
 	"time"
 	"fmt"
@@ -9,17 +14,22 @@ import (
 )
 
 
-var VERSION string = "0.0.1"
+var VERSION string = "0.1.0"
+var locale Locale
+var objects [3]string
 
 
 type Options struct {
 	Version bool
 	Choice  int
+	Lang    string
 }
 
 
 func main(){
 	options := ParseArguments(os.Args[1:])
+
+	locale = GetLocale(options.Lang)
 
 	if options.Version {
 		fmt.Println("v" + VERSION)
@@ -39,12 +49,33 @@ func main(){
 }
 
 
+type Locale struct {
+	Lang struct {Name string; Code string; Cases [3]string}
+	Game struct {Description string}
+	Results struct {Win string; Lose string; Draw string}
+	Objects struct {Rock string; Scissors string; Paper string}
+	Bot struct {Have string}
+}
+
+
+func GetLocale(lang string) (locale Locale){
+	file, _ := ioutil.ReadFile("locale/" + lang + ".yml")
+	locale = Locale{}
+	yaml.Unmarshal(file, &locale)
+
+	return
+}
+
+
 func ParseArguments(arguments []string) (Options) {
 	var defaultOptions Options
+
+	defaultLang := strings.Split(os.Getenv("LANG"), ".")[0]
 	options := defaultOptions
 
     fs := flag.NewFlagSet("main", flag.ExitOnError)
 
+    fs.StringVar(&options.Lang, "l", defaultLang, "Select language")
     fs.BoolVar(&options.Version, "v", false, "Show version")
     fs.IntVar(&options.Choice, "c", 0, "Select choice")
 
@@ -55,13 +86,16 @@ func ParseArguments(arguments []string) (Options) {
 
 
 func Game(user int){
-    objects := [3]string{"rock", "scissors", "paper"};
-
 	rand.Seed(time.Now().UnixNano())
 	var bot int = rand.Intn(3)
 
 	result := Battle(user, bot)
-	fmt.Println(result.Color, result.Name + "!\033[0m", "Bot has chosen", objects[bot])
+	fmt.Println(
+		result.Color,
+		result.Name + "!\033[0m",
+		locale.Bot.Have + locale.Lang.Cases[bot],
+		objects[bot],
+	)
 }
 
 
@@ -74,7 +108,13 @@ type Result struct {
 
 func Battle(user int, bot int) (result Result){
     matrix := [3][3]int8{{2, 0, 1}, {1, 2, 0}, {0, 1, 2}}
-    events := [3]string{"Win", "Lose", "Draw"}
+    // events := [3]string{"Win", "Lose", "Draw"}
+    events := [3]string{
+    	locale.Results.Win,
+    	locale.Results.Lose,
+    	locale.Results.Draw,
+    }
+
     colors := [3]string{"\033[32m", "\033[31m", "\033[33m"}
 
     for i := 0; i < 3; i++ {
@@ -93,34 +133,54 @@ func Battle(user int, bot int) (result Result){
 
 
 func Logo(){
+	LOGO_LEN   := 37              // Max len of line in logo
+	LETTER_LEN := 8               // Len of bottom part of g
+
 	fmt.Println("\033[1m\033[34m                         _")
 	fmt.Println("  __ _  ___  _ __   ___ | |__  _   _")
 	fmt.Println(" / _` |/ _ \\| '_ \\ / _ \\| '_ \\| | | |")
 	fmt.Println("| (_| | (_) | | | | (_) | |_) | |_| |")
 	fmt.Println(" \\__, |\\___/|_| |_|\\___/|_.__/ \\__,_|")
-	fmt.Println(" |___/\033[0m           Kanobu written in go \n")
+
+	description := locale.Game.Description 
+	descriptionLen := len(strings.Split(description, ""))
+
+	spaceLen := LOGO_LEN - (LETTER_LEN + descriptionLen)
+	space := ""
+
+	for i := 0; i <= spaceLen; i++ {
+		space += " "
+	}
+
+	fmt.Println(" |___/ \033[0m" + space + locale.Game.Description + "\n")
 }
 
 
 func Enter() (user int){
-	fmt.Println("\033[37m1.\033[0m Rock")
-	fmt.Println("2. Scissors")
-	fmt.Println("3. Paper")
+    objects = [3]string{
+    	locale.Objects.Rock,
+    	locale.Objects.Scissors,
+    	locale.Objects.Paper,
+    }
+
+    for i := 0; i < 3; i++ {
+    	fmt.Println(
+    		strconv.Itoa(i + 1) + ".",
+    		strings.Title(objects[i]),
+    	)
+    }
+
 	fmt.Printf(">>> ")
-
 	fmt.Scanf("%d\n", &user)
-
 	fmt.Println()
 
-	user = user - 1
+	user--
 
 	if user < 0 {
 		fmt.Println("[\033[31mERROR\033[0m] user < 0")
-		fmt.Println("[\033[33mNOTE\033[0m]  Enter number from 1 until 3.")
 		os.Exit(0)
 	} else if user > 2 {
 		fmt.Println("[\033[31mERROR\033[0m] user < 2")
-		fmt.Println("[\033[33mNOTE\033[0m]  Enter number from 1 until 3.")
 		os.Exit(0)
 	}
 
